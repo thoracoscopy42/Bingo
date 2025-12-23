b3d709cf4a9da94ea3cc6d8780bef2e1fefda226
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2025-12-23 19:11:51 +0100

    dodałem view do landing page-a i dodałem url w urlsach

--- bingo/views.py
+++ bingo/views.py
@@ -1,3 +1,9 @@
+# Create your views here.
+
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
