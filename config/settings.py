38167f0813b663ad24b3fbf8aad1e55e88f29ef8
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2025-12-24 14:54:43 +0100

    a

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bingo',
-    # 'bingo.apps.BingoConfig',
+    # 'bingo',
+    'bingo.apps.BingoConfig',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'bingo.middleware.ForcePasswordChangeMiddleware',
+    'bingo.middleware.ForcePasswordChangeMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
