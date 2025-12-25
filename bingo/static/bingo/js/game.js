81d265500d47b7a3b9e1ba28e5e5e4e944bdcdf5
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2025-12-25 13:18:16 +0100

    order of function change

--- bingo/static/bingo/js/game.js
+++ bingo/static/bingo/js/game.js
@@ -239,8 +239,8 @@
 
     if (resp.ok) {
       showToast?.("MAMY TO YIPIEE!!!", "success", 2200);
-      burstConfetti(120);
       playAudio("SaveSound", {volume: 0.2});
+      burstConfetti(120);
     } else {
       const txt = await resp.text();
       showToast?.("Are you serious right meow :(" + (txt || ""), "error", 2600);
